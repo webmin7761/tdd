70b20dfdfedb79c5299b379ef680cb0a2b57c54b
webmin <wu.diablo@gmail.com>
2022-03-27 16:41:34 +0800

    1. 其余单选项转换 2. 支持多选项

--- src/main/java/geektime.tdd.args/Args.java
+++ src/main/java/geektime.tdd.args/Args.java
@@ -8,13 +8,36 @@ import java.util.List;
 public class Args {
     public static <T> T parse(Class<T> optionsClass, String... args) {
         try {
+            List<String> arguments = Arrays.asList(args);
+
             Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
             Parameter parameter = constructor.getParameters()[0];
-            Option option = parameter.getAnnotation(Option.class);
-            List<String> arguments = Arrays.asList(args);
-            return (T) constructor.newInstance(arguments.contains("-" + option.value()));
+
+            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(arguments, it)).toArray();
+
+            return (T) constructor.newInstance(values);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    private static Object parseOption(List<String> arguments, Parameter parameter) {
+        Object value = null;
+        Option option = parameter.getAnnotation(Option.class);
+        if (parameter.getType() == boolean.class) {
+            value = arguments.contains("-" + option.value());
+        }
+
+        if (parameter.getType() == int.class) {
+            int index = arguments.indexOf("-" + option.value());
+            value = Integer.parseInt(arguments.get(index + 1));
+        }
+
+        if (parameter.getType() == String.class) {
+            int index = arguments.indexOf("-" + option.value());
+            value = String.valueOf(arguments.get(index + 1));
+        }
+
+        return value;
     }
 }
--- src/test/java/geektime.tdd.args/ArgsTest.java
+++ src/test/java/geektime.tdd.args/ArgsTest.java
@@ -39,10 +39,36 @@ public class ArgsTest {
     static record BooleanOption(@Option("l") boolean logging) {
     }
 
-    //TODO: Integer -p
-    //TODO: String -d
+    @Test
+    public void should_parse_int_as_option_value() {
+        IntOption option = Args.parse(IntOption.class, "-p", "8080");
+        assertEquals(8080, option.port());
+    }
+
+    static record IntOption(@Option("p") int port) {
+    }
+
+    @Test
+    public void should_get_string_as_option_value() {
+        StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
+        assertEquals("/usr/logs", option.directory());
+    }
+
+    static record StringOption(@Option("d") String directory) {
+    }
+
     //多选项单参数
-    //TODO: -l -p 8080 -d /usr/logs
+    @Test
+    public void should_parse_multi_options() {
+        MultiOptions multiOptions = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(multiOptions.logging());
+        assertEquals(8080, multiOptions.port());
+        assertEquals("/usr/logs", multiOptions.directory());
+    }
+
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+
+    }
 
     //边界检查
     //TODO: -bool -l t / -l t f
@@ -54,14 +80,6 @@ public class ArgsTest {
     //TODO: int 0
     //TODO: String ""
 
-    @Test
-    @Disabled
-    public void should_example_one() {
-        Options options = Args.parse(Options.class, "-l -p 8080 -d /usr/logs");
-        assertTrue(options.logging());
-        assertEquals(8080, options.port());
-        assertEquals("/usr/logs", options.directory());
-    }
 
     //-g this is a list -d 1 2 -3 5
     @Test
@@ -70,10 +88,6 @@ public class ArgsTest {
         ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
         assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
         assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
-    }
-
-    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
-
     }
 
     static record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
