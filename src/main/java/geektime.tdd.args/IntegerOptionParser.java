14ae86487957dff7a4bdc8788adc59e529d691cb
webmin <wu.diablo@gmail.com>
2022-03-27 19:18:39 +0800

    采用抽父类，子类覆盖方式重构

--- src/main/java/geektime.tdd.args/IntegerOptionParser.java
+++ src/main/java/geektime.tdd.args/IntegerOptionParser.java
@@ -7,6 +7,10 @@ class IntegerOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        return parseValue(arguments, index);
+    }
+
+    protected Object parseValue(List<String> arguments, int index) {
         return Integer.parseInt(arguments.get(index + 1));
     }
 }
--- src/main/java/geektime.tdd.args/StringOptionParser.java
+++ src/main/java/geektime.tdd.args/StringOptionParser.java
@@ -2,11 +2,10 @@ package geektime.tdd.args;
 
 import java.util.List;
 
-class StringOptionParser implements OptionParser {
+class StringOptionParser extends IntegerOptionParser {
 
     @Override
-    public Object parse(List<String> arguments, Option option) {
-        int index = arguments.indexOf("-" + option.value());
+    protected Object parseValue(List<String> arguments, int index) {
         return String.valueOf(arguments.get(index + 1));
     }
 }
